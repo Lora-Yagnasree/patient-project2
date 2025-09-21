7294bd6f393ba0e29374f0614a0df7198e218bdf
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-09-21 12:14:09 +0530

    ok

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Patient
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
@@ -7,13 +8,17 @@ class PatientSerializer(serializers.ModelSerializer):
 
     IDENTITY_TYPE_CHOICES = ['Aadhar No','PAN','Passport','Driving License','Voter ID','Other']
     INSURANCE_CHOICES = ['HDFC ERGO','ICICI Lombard','Bajaj Allianz','Star Health','Other']
+
     def validate_identities(self, value):
+        if value is None:
+            return []  # default to empty list if nothing sent
         for entry in value:
             if 'identity_type' not in entry or 'identity_number' not in entry:
                 raise serializers.ValidationError("Each identity must have type and number.")
             if entry['identity_type'] not in self.IDENTITY_TYPE_CHOICES:
                 raise serializers.ValidationError(f"Invalid identity_type: {entry['identity_type']}")
         return value
+
     def validate_insurance(self, value):
         if value is None:
             return []  # default to empty list if nothing sent
@@ -23,4 +28,3 @@ class PatientSerializer(serializers.ModelSerializer):
             if entry['insurance_company'] not in self.INSURANCE_CHOICES:
                 raise serializers.ValidationError(f"Invalid insurance_company: {entry['insurance_company']}")
         return value
-
